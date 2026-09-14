938f3eac78fa08eef9b3deed5f75ea1770e4145e
samataSeoappebsoft <samatanandy27@gmail.com>
2026-09-14 14:50:05 +0530

    Describe your changes

--- lib/questions_summery.dart
+++ lib/questions_summery.dart
@@ -7,26 +7,30 @@ class QuestionsSummery extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Column(
-      children: summeryData.map((data) {
-        return Row(
-          children: [
-            Text(
-              ((data['questions_index'] as int) + 1).toString(),
-            ),
-          Expand (
-             Child:Column(
-              children: [
-                Text(data['question'] as String),
-                const SizedBox(height: 5),
-                Text(data['user_answers'] as String),
-                Text(data['correct_answers'] as String),
-              ],
-            ),
-          )     
-          ],
-        );
-      }).toList(),
+    return SizedBox(
+      height: 300,
+      child: Column(
+        children: summeryData.map((data) {
+          return Row(
+            children: [
+              Text(
+                ((data['questions_index'] as int) + 1).toString(),
+              ),
+               Expanded(
+                 child: Column(
+                  children: [
+                    Text(data['question'] as String),
+                    const SizedBox(height: 5),
+                    Text(data['user_answers'] as String),
+                    Text(data['correct_answers'] as String),
+                  ],
+                             ),
+               ),
+                
+            ],
+          );
+        }).toList(),
+      ),
     );
   }
 }
